1943fe8e162d9e4348120e560dc4213b9987c2a9
drp_3 <drp_3@DESKTOP-T87EQCP>
2024-02-19 23:02:55 -0600

    Agregar archivos de proyecto.

---
+++ Unit 2 prac 4/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Vehiculo
+{
+    public string marca { get; set; }
+
+    public void Arrancar()
+    {
+        Console.WriteLine(marca + " está arrancando.");
+    }
+}
+
+public class Coche : Vehiculo
+{
+    public void TocarBocina()
+    {
+        Console.WriteLine(marca + " está tocando la bocina.");
+    }
+}
+
+public class Motocicleta : Vehiculo
+{
+    public void Acelerar()
+    {
+        Console.WriteLine(marca + " está acelerando.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Coche miCoche = new Coche();
+        miCoche.marca = "Toyota";
+        miCoche.Arrancar();
+        miCoche.TocarBocina();
+
+        Motocicleta miMoto = new Motocicleta();
+        miMoto.marca = "Honda";
+        miMoto.Arrancar();
+        miMoto.Acelerar();
+    }
+}
+
